3ee479b4c35d7f0eb322db4804c3ee232b6eb6c5
Achal-Aggarwal <theachalaggarwal@gmail.com>
2016-01-26 22:14:21 +0530

    Completing word count spark example.

--- src/main/java/com/tw/example/SparkExample.java
+++ src/main/java/com/tw/example/SparkExample.java
@@ -1,38 +1,60 @@
 package com.tw.example;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.spark.*;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class SparkExample {
 
-  private static final Configuration configuration = new Configuration();
+  public static void run(JavaSparkContext jsp, String inputDir, String outputDir) {
+    System.out.println("Taking input dir: " + inputDir);
+    JavaRDD<String> textFileJavaRdd = jsp.textFile(inputDir);
 
-  public static void main(String[] args){
+    JavaPairRDD<String, Long> wordCountJavaRdd = textFileJavaRdd
+      .flatMap(splitLineBySpace())
+      .mapToPair(createPairOfWordAsKeyAndOneAsValue())
+      .reduceByKey(combineBySummingValue());
 
-
-    System.out.println("Taking input dir: " + args[0]);
-    SparkConf sc= new SparkConf().setAppName("MR");
-    SparkContext sContext = new SparkContext(sc);
-    JavaSparkContext jsp = new JavaSparkContext(sContext);
-    long wordCount = jsp.newAPIHadoopFile(args[0], TextInputFormat.class, LongWritable.class,Text.class,configuration).mapValues(getWords()).count();
-
-
+    System.out.println("Writing to output dir: " + outputDir);
+    wordCountJavaRdd.saveAsTextFile(outputDir);
   }
 
-  private static Function<Text, String> getWords() {
-    return new Function<Text, String>() {
-      public String call(Text v1) throws Exception {
-        System.out.println("Word: " + v1.toString());
-        return v1.toString();
+  private static FlatMapFunction<String, String> splitLineBySpace() {
+    return new FlatMapFunction<String, String>() {
+      public Iterable<String> call(String line) throws Exception {
+        return Arrays.asList(line.split(" "));
       }
     };
+  }
+
+  private static PairFunction<String, String, Long> createPairOfWordAsKeyAndOneAsValue() {
+    return new PairFunction<String, String, Long>() {
+      public Tuple2<String, Long> call(String word) throws Exception {
+        return new Tuple2<String, Long>(word, 1L);
+      }
+    };
+  }
+
+  private static Function2<Long, Long, Long> combineBySummingValue() {
+    return new Function2<Long, Long, Long>() {
+      public Long call(Long v1, Long v2) throws Exception {
+        return v1 + v2;
+      }
+    };
+  }
+
+  public static void main(String[] args){
+    SparkConf sc = new SparkConf().setAppName("WordCount");
+    JavaSparkContext jsp = new JavaSparkContext(new SparkContext(sc));
+
+    run(jsp, args[0], args[1]);
   }
 }
