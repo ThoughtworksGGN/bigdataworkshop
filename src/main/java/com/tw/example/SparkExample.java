b90e645ddf71267c15556062d2b5c18f64f4a8a6
Vinay <vinaymor@thoughtworks.com>
2016-01-28 15:42:28 +0530

    |#000| chandan,achal,vishal| word count skelton for spark

--- src/main/java/com/tw/example/SparkExample.java
+++ src/main/java/com/tw/example/SparkExample.java
@@ -15,41 +15,9 @@ import java.util.Arrays;
 public class SparkExample {
 
   public static void run(JavaSparkContext jsp, String inputDir, String outputDir) {
-    System.out.println("Taking input dir: " + inputDir);
-    JavaRDD<String> textFileJavaRdd = jsp.textFile(inputDir);
-
-    JavaPairRDD<String, Long> wordCountJavaRdd = textFileJavaRdd
-      .flatMap(splitLineBySpace())
-      .mapToPair(createPairOfWordAsKeyAndOneAsValue())
-      .reduceByKey(combineBySummingValue());
-
-    System.out.println("Writing to output dir: " + outputDir);
-    wordCountJavaRdd.saveAsTextFile(outputDir);
+    //write your logic here
   }
 
-  private static FlatMapFunction<String, String> splitLineBySpace() {
-    return new FlatMapFunction<String, String>() {
-      public Iterable<String> call(String line) throws Exception {
-        return Arrays.asList(line.split(" "));
-      }
-    };
-  }
-
-  private static PairFunction<String, String, Long> createPairOfWordAsKeyAndOneAsValue() {
-    return new PairFunction<String, String, Long>() {
-      public Tuple2<String, Long> call(String word) throws Exception {
-        return new Tuple2<String, Long>(word, 1L);
-      }
-    };
-  }
-
-  private static Function2<Long, Long, Long> combineBySummingValue() {
-    return new Function2<Long, Long, Long>() {
-      public Long call(Long v1, Long v2) throws Exception {
-        return v1 + v2;
-      }
-    };
-  }
 
   public static void main(String[] args){
     SparkConf sc = new SparkConf().setAppName("WordCount");
