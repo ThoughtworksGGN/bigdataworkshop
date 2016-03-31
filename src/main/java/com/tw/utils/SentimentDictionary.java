85ed37dc2bff3c682386c4e6c25826eae20efc4a
Achal-Aggarwal <theachalaggarwal@gmail.com>
2016-03-31 14:35:54 +0530

    Adding dictionary.

---
+++ src/main/java/com/tw/utils/SentimentDictionary.java
@@ -0,0 +1,45 @@
+package com.tw.utils;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SentimentDictionary
+{
+  private Map<String,Integer> map = new HashMap<String, Integer>();
+
+  private static SentimentDictionary instance= null;
+
+  public static SentimentDictionary getInstance() throws IOException {
+    if(instance == null){
+      instance = buildDictionary();
+    }
+    return instance;
+  }
+
+  private static SentimentDictionary buildDictionary() throws IOException {
+    SentimentDictionary sentimentDictionary = new SentimentDictionary();
+    BufferedReader br = new BufferedReader(new FileReader(
+      new File(SentimentDictionary.class.getClassLoader().getResource("dictionary.tff").getFile())
+    ));
+
+    String line;
+
+    while((line = br.readLine()) != null)
+    {
+      String [] words = line.split(" ");
+      sentimentDictionary.map.put(words[0], "y".equals(words[1]) ? 1 : -1);
+    }
+
+    return sentimentDictionary;
+  }
+
+  public Integer getSentiment(String word){
+    if(map.containsKey(word)){
+      return map.get(word);
+    }
+
+    return 0;
+  }
+}
+
